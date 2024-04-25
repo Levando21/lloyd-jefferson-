02d88649f3d1ba06fc369449941d7c628cee6e62
Lev <leozolotukhin@gmail.com>
2024-04-25 23:03:29 +0200

    update main js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './js/header.js';
+import './js/hero.js';
+
+import './js/about-us.js';
+
+import './js/projects.js';
+
+import './js/faq.js';
+import './js/covers.js';
+import './js/review.js';
+import './js/work-together.js';
