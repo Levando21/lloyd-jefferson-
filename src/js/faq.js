47a935b8809a13bde8cc0a84a09cd3b153064af6
Maryna Binko <benyhorses@hotmail.com>
2024-04-26 14:43:18 +0100

    responsive layout for screens up to 1440px
    - Adjusted styles to ensure proper display on screens up to 1440px width
    - Updated CSS to accommodate responsive design requirements

---
+++ src/js/faq.js
@@ -0,0 +1,25 @@
+// import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
+
+// // Default options
+// new Accordion('.container-first');
+
+// // User options
+// new Accordion('.container-second', {
+//   duration: 400,
+//   showMultiple: true,
+//   onOpen: function(currentElement) {
+//     console.log(currentElement);
+//   }
+// });
+
+// // Define several accordions with the same options (pass an array with selectors)
+// new Accordion(['.container-first', '.container-second'], {});
+
+// // or pass an array with HTMLElements
+// const accordions = Array.from(document.querySelectorAll('.accordion-container'));
+// new Accordion(accordions, {});
+
+// // Detach events
+// const accordion = new Accordion('.container-first');
+// accordion.detachEvents();
