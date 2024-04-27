2b4348290ca9aef05309cb614d2b6358cc30db20
Yilchik <yulyayulia2@gmail.com>
2024-04-27 20:55:03 +0200

    gap css

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,25 +1,25 @@
-const coversSection = document.querySelector('.covers');
-const coversLine = document.querySelectorAll('.covers-line');
-console.log(coversLine);
+// const coversSection = document.querySelector('.covers');
+// const coversLine = document.querySelectorAll('.covers-line');
+// console.log(coversLine);
 
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.5,
-};
+// const options = {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: 0.5,
+// };
 
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      coversLine.forEach(line => {
-        line.classList.add('animation');
-      });
-    } else {
-      coversLine.forEach(line => {
-        line.classList.remove('animation');
-      });
-    }
-  });
-}, options);
+// const observer = new IntersectionObserver(function (entries, observer) {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       coversLine.forEach(line => {
+//         line.classList.add('animation');
+//       });
+//     } else {
+//       coversLine.forEach(line => {
+//         line.classList.remove('animation');
+//       });
+//     }
+//   });
+// }, options);
 
-observer.observe(coversSection);
+// observer.observe(coversSection);
