dff6e3f5c3d1efade44ae0b82a4063161ace8b84
Maryna Binko <benyhorses@hotmail.com>
2024-04-28 18:55:46 +0100

    FAQ animation added

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,6 +2,7 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 
+
 document.addEventListener('DOMContentLoaded', function() {  
   const faqItems = document.querySelectorAll('.faq-list');
   faqItems.forEach(function(item) {
@@ -9,25 +10,64 @@ document.addEventListener('DOMContentLoaded', function() {
     const btnDown = item.querySelector('.faq-btn-down');
 
     btnUp.addEventListener('click', function() {
-      togglePanel(item);
+      togglePanel(item, 'up');
     });
  
     btnDown.addEventListener('click', function() {
-      togglePanel(item);
+      togglePanel(item, 'down');
     });
   });
 
- 
-  function togglePanel(item) {
+  function togglePanel(item, direction) {
     const panel = item.querySelector('.faq-panel');
     const btnUp = item.querySelector('.faq-btn-up');
     const btnDown = item.querySelector('.faq-btn-down'); 
-    panel.classList.toggle('hidden');   
-    btnUp.classList.toggle('hidden');
-    btnDown.classList.toggle('hidden');
+
+    if (direction === 'up') {
+      panel.classList.add('hidden');   
+      btnUp.classList.add('hidden');
+      btnDown.classList.remove('hidden');
+      btnDown.classList.remove('slide-out');
+      btnUp.classList.add('slide-in');
+    } else {
+      panel.classList.remove('hidden');   
+      btnUp.classList.remove('hidden');
+      btnDown.classList.add('hidden');
+      btnUp.classList.remove('slide-out');
+      btnUp.classList.remove('slide-in'); 
+      void btnDown.offsetWidth; 
+      btnDown.classList.remove('slide-out'); 
+      btnDown.classList.add('slide-in');
+    }
   }
 });
 
+
+// document.addEventListener('DOMContentLoaded', function() {  
+//   const faqItems = document.querySelectorAll('.faq-list');
+//   faqItems.forEach(function(item) {
+//     const btnUp = item.querySelector('.faq-btn-up');
+//     const btnDown = item.querySelector('.faq-btn-down');
+
+//     btnUp.addEventListener('click', function() {
+//       togglePanel(item);
+//     });
+ 
+//     btnDown.addEventListener('click', function() {
+//       togglePanel(item);
+//     });
+//   });
+
+ 
+//   function togglePanel(item) {
+//     const panel = item.querySelector('.faq-panel');
+//     const btnUp = item.querySelector('.faq-btn-up');
+//     const btnDown = item.querySelector('.faq-btn-down'); 
+//     panel.classList.toggle('hidden');   
+//     btnUp.classList.toggle('hidden');
+//     btnDown.classList.toggle('hidden');
+//   }
+// });
 
 
 
