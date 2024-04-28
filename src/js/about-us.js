3d164057edc5aaf81b1b11cac1cbe2e48850aa08
d-sofiika <soniadrozd2709@gmail.com>
2024-04-28 13:35:56 +0200

    added swiper

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -6,44 +6,76 @@ import '../../node_modules/swiper/swiper.css';
 
 // Accordion //
 
-const accordionsList = document.querySelectorAll(".aboutus-accordion-item");
+const accordion = new Accordion('.accordion', {
+  animationDuration: 300, 
+  singleOpen: true,
+});
 
+const accordionItems = document.querySelectorAll('.aboutus-accordion-item');
 
+accordionItems.forEach(item => {
+  const btnUp = item.querySelector('.aboutus-accord-btn-up');
+  const btnDown = item.querySelector('.aboutus-accord-btn-down');
 
-//const accordions = Array.from(activeText);
-document.addEventListener('DOMContentLoaded', function () {
-    accordionsList.forEach(function (item) {
-    console.log(item);
-    const btnDown = item.querySelector(".aboutus-accord-btn-down");
-    const btnUp = item.querySelector(".aboutus-accord-btn-up");
-        btnDown.addEventListener('click', function () { activity(item); });
-        btnUp.addEventListener('click', function () { activity(item) });
-})
+  btnUp.addEventListener('click', () => {
+  const dropdown = item.querySelector('.aboutus-accord-dropdown');
+  
+  accordion.open(item); 
+  dropdown.classList.remove('hidden'); 
+  setTimeout(() => {
+    btnUp.classList.add('hidden');
+    btnDown.classList.remove('hidden');
+  }, 100); 
+});
 
+btnDown.addEventListener('click', () => {
+  const dropdown = item.querySelector('.aboutus-accord-dropdown');
+  accordion.close(item); 
+  dropdown.classList.add('hidden'); 
+  btnUp.classList.remove('hidden');
+  btnDown.classList.add('hidden');
 
-  function activity(item) {
-    const activeText = item.querySelector(".aboutus-accord-dropdown")
-    const btnDown = item.querySelector(".aboutus-accord-btn-down");
-    const btnUp = item.querySelector(".aboutus-accord-btn-up");
-    activeText.classList.toggle('hidden');   
-    btnUp.classList.toggle('hidden');
-    btnDown.classList.toggle('hidden');
-  }
-
-})
+  
+});
+});
 
 // Swiper //
 
+const swiper = new Swiper('.aboutus-skills-swiper', {
+  speed: 400,
+  spaceBetween: 0,
+  loop: true,
+  setWrapperSize: true,
+  breakpoints: {
+// Налаштування для десктопу
+    1280: {
+      slidesPerView: 6,
+    },
+// Налаштування для планшету
+    768: {
+      slidesPerView: 3,
+     
+    },
+// Налаштування для мобільних пристроїв
+    320: {
+      slidesPerView: 2,
+    }
+  }
+  
+  
+});
 const btnNext = document.querySelector('.aboutus-skills-btn');
 
-const swiper = new Swiper('.swiper', {
-  speed: 400,
-    spaceBetween: 100,
-  slidesPerView: 2,
-});
-
 btnNext.addEventListener('click', () => {
-
   swiper.slideNext();
+  
 });
-
+swiper.on('slideChange', () => {
+  if (swiper.params.slidesPerView >= swiper.slides.length) {
+    btnNext.disabled = true;
+    const btnIcon = document.querySelector('.aboutus-skills-btn-icon');
+    btnIcon.style.fill = '#3b3b3b';
+  } else {
+    btnNext.disabled = false;
+  }
+});
