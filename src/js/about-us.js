fa9267aae63c118aae2ec05de6df173e665de884
Vsanotskyi <vsanotskyi@gmail.com>
2024-04-30 02:44:36 +0300

    accordion

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -1,68 +1,18 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import 'accordion-js/dist/accordion.min.css';
 
 import Swiper from 'swiper';
 import '../../node_modules/swiper/swiper.css';
 
 // Accordion //
-const accordionsList = document.querySelectorAll('.aboutus-accordion-item');
-
-document.addEventListener('DOMContentLoaded', function () {
-  accordionsList.forEach(function (item, index) {
-    const activeText = item.querySelector('.aboutus-accord-dropdown');
-    const btnDown = item.querySelector('.aboutus-accord-btn-down');
-    const btnUp = item.querySelector('.aboutus-accord-btn-up');
-    if (index === 0) {
-      activeText.classList.toggle('hidden');
-      activeText.classList.add('active');
-
-      btnDown.classList.add('hidden');
-      btnUp.classList.remove('hidden');
-    }
-    btnUp.addEventListener('mouseenter', function () {
-      titleAccordion.classList.add('hovered');
-    });
-
-    btnUp.addEventListener('mouseleave', function () {
-      titleAccordion.classList.remove('hovered');
-    });
-
-    btnDown.addEventListener('mouseenter', function () {
-      titleAccordion.classList.add('hovered');
-    });
-
-    btnDown.addEventListener('mouseleave', function () {
-      titleAccordion.classList.remove('hovered');
-    });
-
-    btnDown.addEventListener('click', function () {
-      activity(item);
-    });
-    btnUp.addEventListener('click', function () {
-      activity(item);
-    });
-  });
-
-  function activity(item) {
-    const activeText = item.querySelector('.aboutus-accord-dropdown');
-    const btnDown = item.querySelector('.aboutus-accord-btn-down');
-    const btnUp = item.querySelector('.aboutus-accord-btn-up');
-
-    activeText.classList.toggle('active');
-    activeText.classList.toggle('hidden');
-    btnUp.classList.toggle('hidden');
-    btnDown.classList.toggle('hidden');
-    if (activeText.classList.contains('active')) {
-      activeText.style.maxHeight = activeText.scrollHeight + 'px'; // Встановлення максимальної висоти
-    } else {
-      activeText.style.maxHeight = '0'; // Сховати панель акордеону
-    }
-  }
+new Accordion('.accordion-container', {
+  duration: 800,
+  showMultiple: true,
 });
 
 // Swiper //
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   const slides = document.querySelectorAll('.aboutus-skills-circle');
   const swiperWrapper = document.querySelector('.swiper-wrapper');
   slides.forEach((slide, index) => {
@@ -90,11 +40,11 @@ document.addEventListener('DOMContentLoaded', function () {
       },
     },
     on: {
-      init: function () {
+      init: function() {
         document.querySelector('.swiper-slide').style.backgroundColor =
           '#ed3b44';
       },
-      slideChange: function () {
+      slideChange: function() {
         const previousSlide = this.slides[this.previousIndex];
         const activeSlide = this.slides[this.activeIndex];
         previousSlide.style.background = '';
--- src/js/faq.js
+++ src/js/faq.js
@@ -1,5 +1,5 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import 'accordion-js/dist/accordion.min.css';
 
 document.addEventListener('DOMContentLoaded', function () {
   const accordions = Array.from(
