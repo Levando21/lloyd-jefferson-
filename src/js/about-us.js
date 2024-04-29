f998674c2fdd9523d8f03c65cac381280d332cb9
Snigurr <laska135173@gmail.com>
2024-04-29 14:47:54 +0100

    fix pc screen swiper

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -36,44 +36,49 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Swiper //
 
-const swiper = new Swiper('.aboutus-skills-swiper', {
-  speed: 400,
-  spaceBetween: 0,
-  loop: true,
-  setWrapperSize: true,
-  breakpoints: {
-// Налаштування для десктопу
-    1440: {
-      slidesPerView: 6,
-    },
-// Налаштування для планшету
-    768: {
-      slidesPerView: 3,
-    },
-// Налаштування для мобільних пристроїв
-    320: {
-      slidesPerView: 2,
-    }
-  },
-  on: {
-    init: function () {
-     
-      document.querySelector('.swiper-slide').style.backgroundColor = '#ed3b44';
-    },
-    slideChange: function () {
+document.addEventListener('DOMContentLoaded', function () {
+  const slides = document.querySelectorAll('.aboutus-skills-circle');
+  const swiperWrapper = document.querySelector('.swiper-wrapper');
+  slides.forEach((slide, index) => {
+    const clone = slide.cloneNode(true); 
+    swiperWrapper.appendChild(clone); 
+  });
 
-      const previousSlide = this.slides[this.previousIndex];
-      const activeSlide = this.slides[this.activeIndex];
-      previousSlide.style.background = '' ;
-      activeSlide.style.backgroundColor = '#ed3b44';
+  const swiper = new Swiper('.aboutus-skills-swiper', {
+    speed: 400,
+    spaceBetween: 0,
+    loop: true,
+    setWrapperSize: true,
+    breakpoints: {
+      // Налаштування для десктопу
+      1440: {
+        slidesPerView: 6,
+      },
+      // Налаштування для планшету
+      768: {
+        slidesPerView: 3,
+      },
+      // Налаштування для мобільних пристроїв
+      320: {
+        slidesPerView: 2,
+      }
     },
-  },
+    on: {
+      init: function () {
+        document.querySelector('.swiper-slide').style.backgroundColor = '#ed3b44';
+      },
+      slideChange: function () {
+        const previousSlide = this.slides[this.previousIndex];
+        const activeSlide = this.slides[this.activeIndex];
+        previousSlide.style.background = '';
+        activeSlide.style.backgroundColor = '#ed3b44';
+      },
+    },
+  });
 
-});
-  
-    
 
-const btnNext = document.querySelector('.aboutus-skills-btn');
-btnNext.addEventListener('click', () => {
-  swiper.slideNext();
+  const btnNext = document.querySelector('.aboutus-skills-btn');
+  btnNext.addEventListener('click', () => {
+    swiper.slideNext();
+  });
 });
