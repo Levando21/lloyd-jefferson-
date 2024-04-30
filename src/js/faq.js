4f916994a2480dec585dfa40740577cd4322907e
Maryna Binko <benyhorses@hotmail.com>
2024-04-30 16:23:02 +0100

    accordion js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,32 +1,14 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import 'accordion-js/dist/accordion.min.css';
+
+new Accordion(".accordion-container", {
+  duration: 600,
+  showMultiple: true,  
+ });
 
 
-document.addEventListener('DOMContentLoaded', function() {  
-  const faqItems = document.querySelectorAll('.faq-list');
-  faqItems.forEach(function(item) {
-    const btnUp = item.querySelector('.faq-btn-up');
-    const btnDown = item.querySelector('.faq-btn-down');
 
-    btnUp.addEventListener('click', function() {
-      togglePanel(item);
-    });
- 
-    btnDown.addEventListener('click', function() {
-      togglePanel(item);
-    });
-  });
 
- 
-  function togglePanel(item) {
-    const panel = item.querySelector('.faq-panel');
-    const btnUp = item.querySelector('.faq-btn-up');
-    const btnDown = item.querySelector('.faq-btn-down'); 
-    panel.classList.toggle('active');   
-    btnUp.classList.toggle('hidden');
-    btnDown.classList.toggle('hidden');
-  }
-});
 
 
 
