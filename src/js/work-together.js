670e2ef1d36422d7ef3f66d2ddb94e305b888fe1
Vsanotskyi <vsanotskyi@gmail.com>
2024-05-02 05:05:50 +0300

    fix-form, add validator

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,18 +1,71 @@
+import validator from 'validator';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 import {postRequestsApi} from './api.js';
 
 const REGEX = /^\w+(.\w+)?@[a-zA-Z_]+?.[a-zA-Z]{2,3}$/;
 
-const validateEmail = (regex, email) => {
-  return regex.test(email);
-};
+// const validateEmail = (regex, email) => {
+//   return regex.test(email);
+// };
+
+console.log(REGEX.test('user11@mail'))
+
+const testData = {
+  email: 'user11@mail',
+  comment: 'some text'
+}
+
+const testApi = async (data) => {
+  return await postRequestsApi(data)
+}
+
+testApi(testData)
+
+iziToast.settings({
+  position: 'bottomRight',
+})
 
 const formEl = document.getElementById('footer-form');
 const emailEl = document.getElementById('footer-form-input');
 const commentEl = document.getElementById('footer-form-textarea');
 const emailMesEl = document.querySelector('.email-message');
 const commentMesEl = document.querySelector('.comment-message');
+const resBtnEl = document.querySelector('.footer-form-res');
 const modal = document.getElementById('modal');
 const closeButton = document.querySelector('.footer-modal-btn-close');
+
+const toggleStylesValidateEmail = (email) => {
+  if (validator.isEmail(email) !== false) {
+    emailMesEl.textContent = 'Success!';
+    emailMesEl.classList.remove('isErrorMessage');
+    emailMesEl.classList.add('isSuccessMessage');
+  } else {
+    emailMesEl.textContent = 'Invalid email, try again';
+    emailMesEl.classList.remove('isSuccessMessage');
+    emailMesEl.classList.add('isErrorMessage');
+  }
+}
+
+const toggleStylesValidateComment = (comment) => {
+  if (comment.trim().length > 0) {
+    commentMesEl.textContent = 'Success!';
+    commentMesEl.classList.remove('isErrorMessage');
+    commentMesEl.classList.add('isSuccessMessage');
+  } else {
+    commentMesEl.textContent = 'Please, write some text';
+    commentMesEl.classList.remove('isSuccessMessage');
+    commentMesEl.classList.add('isErrorMessage');
+  }
+}
+
+const resetStylesValidate = () => {
+  emailMesEl.textContent = '';
+  commentMesEl.textContent = '';
+  emailMesEl.classList.remove('isSuccessMessage', 'isErrorMessage');
+  commentMesEl.classList.remove('isSuccessMessage', 'isErrorMessage');
+}
 
 const handleOpenModal = data => {
   modal.classList.add('show-modal');
@@ -30,29 +83,17 @@ const handleCloseModal = () => {
 const handleMailCheck = (e) => {
   const email = e.target.value;
 
-  if (validateEmail(REGEX, email) !== false) {
-    emailMesEl.textContent = 'Success!';
-    emailMesEl.classList.remove('isErrorMessage');
-    emailMesEl.classList.add('isSuccessMessage');
-  } else if (email.length > 0) {
-    emailMesEl.textContent = 'Invalid email, try again';
-    emailMesEl.classList.remove('isSuccessMessage');
-    emailMesEl.classList.add('isErrorMessage');
-  }
+  toggleStylesValidateEmail(email)
+
+  resBtnEl.style.display = 'block'
 };
 
 const handleCommentCheck = (e) => {
   const comment = e.target.value;
 
-  if (comment.trim().length > 1) {
-    commentMesEl.textContent = 'Success!';
-    commentMesEl.classList.remove('isErrorMessage');
-    commentMesEl.classList.add('isSuccessMessage');
-  } else {
-    commentMesEl.textContent = 'Please, write some text';
-    commentMesEl.classList.remove('isSuccessMessage');
-    commentMesEl.classList.add('isErrorMessage');
-  }
+  toggleStylesValidateComment(comment)
+
+  resBtnEl.style.display = 'block'
 };
 
 const handleSubmit = async (e) => {
@@ -62,16 +103,28 @@ const handleSubmit = async (e) => {
     email: emailEl.value,
     comment: commentEl.value,
   };
-  // checking email
-  if (validateEmail(REGEX, data.email) === false) return;
 
-  // checking comment
-  if (data.comment.trim().length < 1) return;
+  if (validator.isEmail(data.email) === false) {
+    iziToast.error({
+      title: data.email.length < 1 ? 'Please, write your email' : 'Not validate email',
+    });
 
-  // send data
+    toggleStylesValidateEmail(data.email)
+    return;
+  }
+
+  if (data.comment.trim().length < 1) {
+    iziToast.error({
+      title: 'Write some text',
+    });
+
+    toggleStylesValidateComment(data.comment)
+    return;
+  }
+
   try {
     const result = await postRequestsApi(data);
-    formEl.reset();
+
     handleOpenModal(result);
   } catch (error) {
     handleOpenModal({
@@ -80,17 +133,27 @@ const handleSubmit = async (e) => {
     });
   }
 
-  emailMesEl.textContent = '';
-  commentMesEl.textContent = '';
-  emailMesEl.classList.remove('isSuccessMessage', 'isErrorMessage');
-  commentMesEl.classList.remove('isSuccessMessage', 'isErrorMessage');
+  formEl.reset();
+  resBtnEl.style.display = 'none'
+  resetStylesValidate()
 };
 
-emailEl.addEventListener('blur', handleMailCheck);
+const handleClickResetForm = () => {
+  resBtnEl.style.display = 'none'
 
-commentEl.addEventListener('blur', handleCommentCheck);
+  emailEl.value = ''
+  commentEl.value = ''
+
+  resetStylesValidate()
+}
+
+emailEl.addEventListener('input', handleMailCheck);
+
+commentEl.addEventListener('input', handleCommentCheck);
 
 formEl.addEventListener('submit', handleSubmit);
+
+resBtnEl.addEventListener('click', handleClickResetForm)
 
 closeButton.addEventListener('click', () => {
   handleCloseModal();
